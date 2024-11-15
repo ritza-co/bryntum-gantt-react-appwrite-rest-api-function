143536c9c208c4e59234fcd33eea0e077dabbeb0
Matthew Clarke <m.d.clarke.sa@gmail.com>
2024-11-15 12:43:48 +0200

    add sync tasks and dependencies, update README

--- README.md
+++ README.md
@@ -1,37 +1,32 @@
-# ⚡ Node.js Starter Function
+# ⚡ Bryntum Gantt Node.js REST API Appwrite Function 
 
-A simple starter function. Edit `src/main.js` to get started and create something awesome! 🚀
+## Appwrite collections needed
+
+A database with two collections: tasks and dependencies.The tasks collection needs to have [attributes](https://appwrite.io/docs/products/databases/collections#attributes) that represent the [Bryntum Gantt Task Model fields](https://bryntum.com/products/gantt/docs/api/Gantt/model/TaskModel#fields), which represent a Bryntum Gantt task. The dependencies collection needs to have attributes that represent the [Bryntum Gantt Dependency Model fields](https://bryntum.com/products/gantt/docs/api/Gantt/model/DependencyModel#fields), which represent a Bryntum Gantt dependency.
+
+## 🔒 Environment Variables needed
+
+No environment variables required.
+
+- PROJECT_ID 
+- DATABASE_ID
+- TASKS_COLLECTION_ID
+- DEPENDENCIES_COLLECTION_ID
+
+
+## Authentication
+
+Requires a secret JSON Web Token, created using Appwrite [JWT authentication](https://appwrite.io/docs/products/auth/jwt#jwt), added to the request's "authorization" header. This authentication is added so that only logged in users can view or change data. The client side Bryntum Gantt needs to set up Appwrite authentication so that when a user logs in, a session will be created using the Appwrite client SDK. This allows you to generate a JWT using the session and add it to the "authorization" header when making requests to this function.
 
 ## 🧰 Usage
 
-### GET /ping
+### GET
 
-- Returns a "Pong" message.
+Returns tasks and dependencies data using the [load response structure](https://bryntum.com/products/gantt/docs/guide/Gantt/data/crud_manager_project#load-response-structure) expected by a Bryntum Gantt Crud Manager load request.
 
-**Response**
+### POST
 
-Sample `200` Response:
-
-```text
-Pong
-```
-
-### GET, POST, PUT, PATCH, DELETE /
-
-- Returns a "Learn More" JSON response.
-
-**Response**
-
-Sample `200` Response:
-
-```json
-{
-  "motto": "Build like a team of hundreds_",
-  "learn": "https://appwrite.io/docs",
-  "connect": "https://appwrite.io/discord",
-  "getInspired": "https://builtwith.appwrite.io"
-}
-```
+Handles POST requests from Bryntum Gantt Crud Manager sync requests that include changes for all the linked data stores in a single request, which has a specific [sync request structure](https://bryntum.com/products/gantt/docs/guide/Scheduler/data/crud_manager_in_depth#sync-request-structure). The response has a specific [sync response structure](https://bryntum.com/products/gantt/docs/guide/Gantt/data/crud_manager_project#sync-response-structure) that sends the new `id` of a record created in Appwrite to the client side Bryntum Gantt so that the Bryntum Gantt has the correct `id` for the record.
 
 ## ⚙️ Configuration
 
@@ -42,7 +37,3 @@ Sample `200` Response:
 | Build Commands    | `npm install` |
 | Permissions       | `any`         |
 | Timeout (Seconds) | 15            |
-
-## 🔒 Environment Variables
-
-No environment variables required.
--- src/main.js
+++ src/main.js
@@ -1,6 +1,4 @@
-import { Client, Databases, 
-  // ID, 
-  Query } from 'node-appwrite';
+import { Client, Databases, ID, Query } from 'node-appwrite';
 
 const PROJECT_ID = process.env.PROJECT_ID;
 const DATABASE_ID = process.env.DATABASE_ID;
@@ -15,60 +13,192 @@ export default async ({ req, res }) => {
 
     const databases = new Databases(client);
 
+    async function addTask(task) {
+        const { $id } = await databases.createDocument(
+            DATABASE_ID,
+            TASKS_COLLECTION_ID,
+            ID.unique(),
+            task
+        );
+        return $id;
+    }
+
+    async function removeTask(id) {
+        await databases.deleteDocument(DATABASE_ID, TASKS_COLLECTION_ID, id);
+    }
+
+    async function updateTask(id, task) {
+        await databases.updateDocument(DATABASE_ID, TASKS_COLLECTION_ID, id, task);
+    }
+
+    async function addDependency(dependency) {
+        dependency.type = `${dependency.type}`;
+        delete dependency.from;
+        delete dependency.to;
+        const { $id } = await databases.createDocument(
+            DATABASE_ID,
+            DEPENDENCIES_COLLECTION_ID,
+            ID.unique(),
+            dependency
+        );
+        return $id; 
+    }
+
+    async function removeDependency(id) {
+        await databases.deleteDocument(DATABASE_ID, DEPENDENCIES_COLLECTION_ID, id);
+    }
+
+    async function updateDependency(id, dependency) {
+        await databases.updateDocument(DATABASE_ID, DEPENDENCIES_COLLECTION_ID, id, dependency);
+    }
+
+    function createOperation(added, table) {
+        return Promise.all(
+            added.map(async(record) => {
+                const { $PhantomId, ...data } = record;
+                let id;
+                if (table === 'tasks') {
+                    id = await addTask(data);
+                }
+                if (table === 'dependencies') {
+                    id = await addDependency(data);
+                }
+                // Report to the client that the record identifier has been changed
+                return { $PhantomId, id };
+            })
+        );
+    }
+
+    function deleteOperation(deleted, table) {
+        return Promise.all(
+            deleted.map(({ id }) => {
+                if (table === 'tasks') {
+                    removeTask(id);
+                }
+                if (table === 'dependencies') {
+                    removeDependency(id);
+                }
+            })
+        );
+    }  
+
+    function updateOperation(updated, table) {
+        return Promise.all(
+            updated.map(({ $PhantomId, id, ...data }) => {
+                if (table === 'tasks') {
+                    updateTask(id, data);
+                }
+                if (table === 'dependencies') {
+                    updateDependency(id, data);
+                }
+            })
+        );
+    }
+
+    async function applyTableChanges(table, changes) {
+        let rows;
+        if (changes.added) {
+            rows = await createOperation(changes.added, table);
+        }
+        if (changes.removed) {
+          await deleteOperation(changes.removed, table);
+        }
+        if (changes.updated) {
+            await updateOperation(changes.updated, table);
+        }
+        // new task or dependency ids to send to the client
+        return rows;
+    }
+
     if (req.method === 'OPTIONS') {
-      return res.send('', 200, {
-        'Access-Control-Allow-Origin': 'http://localhost:3000',
-        'Access-Control-Allow-Methods': 'POST, GET, OPTIONS',
-        'Access-Control-Allow-Headers': 'Content-Type, Authorization',
-      })
+        return res.send('', 200, {
+          'Access-Control-Allow-Origin': 'http://localhost:3000',
+          'Access-Control-Allow-Methods': 'POST, GET, OPTIONS',
+          'Access-Control-Allow-Headers': 'Content-Type, Authorization',
+        })
     }
 
     if (req.method === 'GET') {
-      try {
-          const tasksResponse = await databases.listDocuments(
-              DATABASE_ID,
-              TASKS_COLLECTION_ID,
-              [Query.orderAsc('parentIndex')]
-          );
+        try {
+            const tasksResponse = await databases.listDocuments(
+                DATABASE_ID,
+                TASKS_COLLECTION_ID,
+                [Query.orderAsc('parentIndex')]
+            );
 
-          const tasks = tasksResponse.documents.map((task) => {
-              task.id = task.$id;
-              // remove AppWrite-specific fields and fields that are null or undefined
-              const obj = Object.fromEntries(Object.entries(task).filter(([_, v]) => v != null ).filter(([k, _]) => k[0] != '$'));
-              return obj;
-          });
+            const tasks = tasksResponse.documents.map((task) => {
+                task.id = task.$id;
+                // remove AppWrite-specific fields and fields that are null or undefined
+                const obj = Object.fromEntries(Object.entries(task).filter(([_, v]) => v != null ).filter(([k, _]) => k[0] != '$'));
+                return obj;
+            });
 
-          const dependenciesResponse = await databases.listDocuments(
-              DATABASE_ID,
-              DEPENDENCIES_COLLECTION_ID
-          );
+            const dependenciesResponse = await databases.listDocuments(
+                DATABASE_ID,
+                DEPENDENCIES_COLLECTION_ID
+            );
 
-          const dependencies = dependenciesResponse.documents.map((dep) => {
-              dep.id = dep.$id;
-              // remove AppWrite-specific fields and fields that are null or undefined
-              const obj = Object.fromEntries(Object.entries(dep).filter(([_, v]) => v != null).filter(([k, _]) => k[0] != '$'));
-              return obj;
-          });
-      
-          return res.json({
-              success : true,
-              tasks   : {
-                  rows : tasks
-              },
-              dependencies : {
-                  rows : dependencies
-              }
-          }, 200, {
+            const dependencies = dependenciesResponse.documents.map((dep) => {
+                dep.id = dep.$id;
+                // remove AppWrite-specific fields and fields that are null or undefined
+                const obj = Object.fromEntries(Object.entries(dep).filter(([_, v]) => v != null).filter(([k, _]) => k[0] != '$'));
+                return obj;
+            });
+        
+            return res.json({
+                success : true,
+                tasks   : {
+                    rows : tasks
+                },
+                dependencies : {
+                    rows : dependencies
+                }
+            }, 200, {
+                'Access-Control-Allow-Origin': 'http://localhost:3000',
+            });
+        } 
+        catch(err) {
+            return res.json({
+                success : false,
+                message : 'Tasks and dependencies could not be loaded'
+            }, 500, {
+                'Access-Control-Allow-Origin': 'http://localhost:3000',
+            });
+        } 
+    }
+
+    if (req.method === "POST") {
+        const { requestId, tasks, dependencies } = req.body;
+        try {
+            const response = { requestId, success : true };
+            // if task changes are passed
+            if (tasks) {
+                const rows = await applyTableChanges('tasks', tasks);
+                // if got some new data to update client
+                if (rows) {
+                    response.tasks = { rows };
+                }
+            }
+            // if dependency changes are passed
+            if (dependencies) {
+                const rows = await applyTableChanges('dependencies', dependencies);
+                // if got some new data to update client
+                if (rows) {
+                    response.dependencies = { rows };
+                }
+            }
+            return res.json(response, 200, {
+                'Access-Control-Allow-Origin': 'http://localhost:3000',
+            });
+        }
+        catch(err) {
+            return res.json({
+                requestId,
+                success : false,
+                message : 'There was an error syncing the data changes'
+            }, 500, {
               'Access-Control-Allow-Origin': 'http://localhost:3000',
-          });
-
-      } catch(err) {
-          return res.json({
-              success : false,
-              message : 'Tasks and dependencies could not be loaded'
-          }, 500, {
-              'Access-Control-Allow-Origin': 'http://localhost:3000',
-          });
-      } 
+            });
+        }
     }
 };
